787dc1b6030585d05590f49f8539b01571d6a75f
Michele Lacchia <michelelacchia@gmail.com>
2015-10-17 20:51:44 +0200

    test(MoodleSpec): pass to Hspec-attoparsec

--- test/MoodleSpec.hs
+++ test/MoodleSpec.hs
@@ -1,62 +1,62 @@
 module MoodleSpec (spec) where
 
-import Test.HUnit (Assertion)
 import Test.Hspec
+import Test.Hspec.Attoparsec ((~>), shouldParse, shouldFailOn)
 import Data.Text (pack)
-import Data.Attoparsec.Text (parseOnly)
 import Moodle
 
 
-shouldBeR :: Either String MoodleVal -> MoodleVal -> Assertion
-shouldBeR actual expected = actual `shouldBe` Right expected
-
 parse :: String -> Either String MoodleVal
-parse = parseOnly parseExpr . pack
+parse = (~> parseExpr) . pack
 
 spec :: Spec
 spec = do
     describe "parseExpr" $ do
         it "parses a single integer" $
-            parse "2" `shouldBeR` Number 2
+            parse "2" `shouldParse` Number 2
         it "parses a single float" $
-            parse "-2.29" `shouldBeR` Number (-2.29)
+            parse "-2.29" `shouldParse` Number (-2.29)
         it "parses a variable" $
-            parse "{var}" `shouldBeR` Variable "var"
+            parse "{var}" `shouldParse` Variable "var"
         it "parses a variable II" $
-            parse "{2var}" `shouldBeR` Variable "2var"
+            parse "{2var}" `shouldParse` Variable "2var"
         it "parses func with no args" $
-            parse "pi()" `shouldBeR` Function "pi" []
+            parse "pi()" `shouldParse` Function "pi" []
         it "parses func with 1 arg" $
-            parse "log(239)" `shouldBeR` Function "log" [Number 239]
+            parse "log(239)" `shouldParse` Function "log" [Number 239]
         it "parses func with 2 args" $
-            parse "sqrt(23, 4)" `shouldBeR`
+            parse "sqrt(23, 4)" `shouldParse`
                 Function "sqrt" [Number 23, Number 4]
         it "parses single addition" $
-            parse "2 + -4" `shouldBeR` Op "+" (Number 2) (Number (-4))
+            parse "2 + -4" `shouldParse` Op "+" (Number 2) (Number (-4))
         it "parses single multiplication" $
-            parse "-2 * 4" `shouldBeR` Op "*" (Number (-2)) (Number 4)
+            parse "-2 * 4" `shouldParse` Op "*" (Number (-2)) (Number 4)
         it "parses single subtraction" $
-            parse "2 - -4" `shouldBeR` Op "-" (Number 2) (Number (-4))
+            parse "2 - -4" `shouldParse` Op "-" (Number 2) (Number (-4))
         it "parses single division" $
-            parse "0 / 32" `shouldBeR` Op "/" (Number 0) (Number 32)
+            parse "0 / 32" `shouldParse` Op "/" (Number 0) (Number 32)
         it "parses add/sub assocl" $
-            parse "2 + 4 - 3" `shouldBeR`
+            parse "2 + 4 - 3" `shouldParse`
                 Op "-" (Op "+" (Number 2) (Number 4)) (Number 3)
         it "parses mul/div assocl" $
-            parse "2 / 4 * 45" `shouldBeR`
+            parse "2 / 4 * 45" `shouldParse`
                 Op "*" (Op "/" (Number 2) (Number 4)) (Number 45)
         it "parses exp assocr" $
-            parse "2^3^4" `shouldBeR`
+            parse "2^3^4" `shouldParse`
                 Op "^" (Number 2) (Op "^" (Number 3) (Number 4))
         it "parses with no spaces" $
-            parse "2+3/{va}" `shouldBeR`
+            parse "2+3/{va}" `shouldParse`
                 Op "+" (Number 2) (Op "/" (Number 3) (Variable "va"))
         it "parses with lots of spaces" $
-            parse "  2  +   3  /  {  va  }  " `shouldBeR`
+            parse "  2  +   3  /  {  va  }  " `shouldParse`
                 Op "+" (Number 2) (Op "/" (Number 3) (Variable "va"))
         it "parses with lots of spaces in function calls" $
-            parse "2 + log ( 24 , 249 )" `shouldBeR`
+            parse "2 + log ( 24 , 249 )" `shouldParse`
                 Op "+" (Number 2) (Function "log" [Number 24, Number 249])
+        it "fails on an empty string" $
+            parseExpr `shouldFailOn` pack ""
+        it "fails on gibberish" $
+            parseExpr `shouldFailOn` pack "q 28nureamijf sdk"
     describe "toLatex" $ do
         it "does not group a positive number" $
             toLatex (Number 4) `shouldBe` "4"
